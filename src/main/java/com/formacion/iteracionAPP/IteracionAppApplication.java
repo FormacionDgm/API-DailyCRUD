d1c1cd2d1ccf03b6d0aa674dc890a0fe3f294f52
David Gómez <davidgmrm@gmail.com>
2018-08-19 13:38:56 +0200

    Start the iteration project

---
+++ src/main/java/com/formacion/iteracionAPP/IteracionAppApplication.java
@@ -0,0 +1,12 @@
+package com.formacion.iteracionAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IteracionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IteracionAppApplication.class, args);
+	}
+}
